bc422ae5c693ef73d21118d44e4a038370ef8c9b
Five Hughes LLC <vertical.consulting17@gmail.com>
2026-02-23 22:44:20 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,6 +21,7 @@ export default function Home() {
       <ServicesPreview />
       <FeaturedProjects />
       <TestimonialsCarousel />
+      <GoogleReviews />
       <CTABanner />
     </div>
   );
