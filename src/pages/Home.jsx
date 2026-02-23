cf9d97d63a9849be2059e0a970ee6ed58a145522
Five Hughes LLC <vertical.consulting17@gmail.com>
2026-02-23 22:44:38 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import ServicesPreview from "@/components/home/ServicesPreview";
 import FeaturedProjects from "@/components/home/FeaturedProjects";
 import TestimonialsCarousel from "@/components/home/TestimonialsCarousel";
 import CTABanner from "@/components/home/CTABanner";
+import GoogleReviews from "@/components/home/GoogleReviews";
 
 export default function Home() {
   return (
